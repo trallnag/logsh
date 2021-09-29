a62c4250aff51e2ba89df30d47e324795e87d4dc
Tim Schwenke <tim.schwenke@trallnag.com>
2021-09-29 11:47:30 +0200

    style: Adjust hlines

--- log.sh
+++ log.sh
@@ -1,41 +1,41 @@
 #!/bin/sh
 
-# ==============================================================================
-#
-# Logsh. A minimal POSIX compliant logging library.
+# ------------------------------------------------------------------------------
 
+# Logsh. A minimal POSIX compliant logging library.
+#
 # Usage:
 #     Either source the content of this file or just place it directly
 #     into your own script. Several functions will be available to you.
-
+#
 #     - logdebug
 #     - loginfo
 #     - logsuccess
 #     - logwarning
 #     - logerror
 #     - logexit
-
+#
 # Configuration:
 #     Logging is configured via environment variables.
-
+#
 #     LOGSH_COLOR:
 #         Can either be 'true' or 'false' and defaults to 'true'. Only
 #         respected while sourcing logsh. This means you can only
 #         switch between 'true' and 'false' by resourcing the script.
-
+#
 #     LOGSH_LEVEL:
 #         Integer ranging from 0 to 4. Defaults to 3. If the log
 #         message level is lower or equal to the selected level,the
 #         log message wil be written to stderr. Here are the mappings
 #         from integers to text levels:
-
+#
 #         0=ERROR
 #         1=WARNING
 #         2=SUCCESS
 #         3=INFO
 #         4=DEBUG
-#
-# ==============================================================================
+
+# ------------------------------------------------------------------------------
 
 if [ "${LOGSH_COLOR:-true}" = "true" ]; then
     _logsh_default_color='\033[0m'
@@ -75,3 +75,5 @@ logsuccess() { log "$1" 2 "SUCCESS" "$_logsh_success_color"; }
 loginfo() { log "$1" 3 "INFO" "$_logsh_info_color"; }
 
 logdebug() { log "$1" 4 "DEBUG" "$_logsh_debug_color"; }
+
+# ------------------------------------------------------------------------------
